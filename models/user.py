a1e5c699d516f35541f08739ae27671dc358fbf2
laamyry <slamiri10@gmail.com>
2023-09-20 00:24:03 +0100

    laamyry py

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__= 'users'
 
-    email = Column(String(255), nullable=False)
-    password = Column(String(255), nullable=False)
-    first_name = Column(String(255), nullable=False)
-    last_name = Column(String(255), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
