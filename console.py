627214cc30a5c8bd2a99c1adcb3ae06010dad8f0
laamyry <slamiri10@gmail.com>
2023-09-20 18:49:33 +0100

    laamyry py

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -125,19 +125,19 @@ class HBNBCommand(cmd.Cmd):
         if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
+
         ps_obj = {}
 
         while i < len(params):
             ps_parts = params[i].split('=')
-            if len(ps_parts) == 2:
+            if len(ps_parts) != 2:
                 i += 1
                 continue
-            
+
             key, value = ps_parts
             key = key.replace('_', ' ')
 
-            if value.startswith('"') and value.endswith('"') :
+            if value.startswith('"') and value.endswith('"'):
                 value = value[1:-1].replace('\\', '"')
 
             if '.' in value:
@@ -350,6 +350,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
