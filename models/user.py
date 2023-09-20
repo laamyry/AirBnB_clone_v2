c116c2d7ea9af0f75376753fea7a420ab3e2d427
laamyry <slamiri10@gmail.com>
2023-09-20 01:09:17 +0100

    laamyry py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel,Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from models import storage_type
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
