ed2f335afb06a8ab4a2c8043484c80f5b7c7ef74
laamyry <slamiri10@gmail.com>
2023-09-20 01:06:50 +0100

    laamyry py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel,Base
 from sqlalchemy import Column, String
 from models import storage_type
 
@@ -8,7 +8,15 @@ from models import storage_type
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        
